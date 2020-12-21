146d62fa92653bf1cf9dc0039e0ac8beffecc857
hsteinmueller <harald.steinmueller@gmail.com>
2020-12-21 22:01:57 +0100

    fixed pdf for mobile

--- components/pdfViewer.js
+++ components/pdfViewer.js
@@ -6,8 +6,17 @@ const PdfViewer = ({ pdf }) => {
       className={styles.object}
       type="application/pdf"
       data={pdf + "#view=Fit"}
-      // data="/resume_en.pdf#view=Fit"
-    />
+    >
+      <p>Looks like your browser doesn't have a PDF plugin.</p>
+      <p>
+        Click{" "}
+        <a href={pdf} download>
+          HERE
+        </a>{" "}
+        to download the file instead.
+      </p>
+    </object>
+    // data="/resume_en.pdf#view=Fit"
   );
 };
 
